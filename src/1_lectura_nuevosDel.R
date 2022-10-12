3dd073d1a9a910f9c2a838d051239e39a75a2aa6
Erick Fajardo <erickfama@outlook.es>
2022-10-12 15:26:15 -0500

    Lectura de datos previos fiscalia

--- src/1_lectura_nuevosDel.R
+++ src/1_lectura_nuevosDel.R
@@ -3,12 +3,22 @@
 # Librerías
 library(magrittr)
 
-# Lectura
-del_raw <- readxl::read_xlsx("./data/1_raw/ConsultaViolencias2017-2022 CIDE.xlsx") %>%
+# Lectura ----
+
+# Datos nuevos
+
+## fiscalia
+carp_fisc_new_raw <- readxl::read_xlsx("./data/1_raw/ConsultaViolencias2017-2022 CIDE.xlsx") %>%
   janitor::clean_names()
 
+## Poder judicial
 judicial1_raw <- readxl::read_xls("./data/1_raw/RESUMEN_FINAL_V01 (1).xls") %>%
   janitor::clean_names()
 
 judicial2_raw <- readxl::read_xls("./data/1_raw/RESUMEN_FINAL_V01 (2).xls") %>%
   janitor::clean_names()
+
+# Datos viejos
+
+## fiscalia
+carp_fisc_old <- readr::read_csv("./data/3_final/carp_clean.csv")
